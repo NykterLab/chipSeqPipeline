6f90e5dcb41740ebf0433a9a52bbc3944cb6b14b
thomas_gnv <thomas.bersez@geneviatechnologies.com>
2021-02-18 10:48:46 +0200

    update container build

--- singularity/buildContainers.sh
+++ singularity/buildContainers.sh
@@ -2,3 +2,5 @@
 # BUILD PIPELINE CONTAINERS ----------------------------------------------------
 mkdir -p build/ logs/ test/
 sudo singularity build build/base recipes/base.rcp > logs/base.log
+sudo singularity build build/bwa recipes/bwa.rcp > logs/bwa.log
+sudo singularity build build/picard recipes/picard.rcp > logs/picard.log
