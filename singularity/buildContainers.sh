6dfd0d9b98a897ccc12509b48865dd03dc3d09a9
thomas_gnv <thomas.bersez@geneviatechnologies.com>
2021-02-22 13:10:29 +0200

    macs2 container

--- singularity/buildContainers.sh
+++ singularity/buildContainers.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
+# pwd: oichWVBUc87u9wS
 # BUILD PIPELINE CONTAINERS ----------------------------------------------------
 mkdir -p build/ logs/ test/
 sudo singularity build build/base recipes/base.rcp > logs/base.log
 sudo singularity build build/bwa recipes/bwa.rcp > logs/bwa.log
 sudo singularity build build/picard recipes/picard.rcp > logs/picard.log
+sudo singularity build build/macs2 recipes/macs2.rcp > logs/macs2.log
